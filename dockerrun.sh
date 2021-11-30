5a3469e1cc063c4e7b6ed36da8e1441bebb6880d
qsobad <qsobad@gmail.com>
2021-11-30 11:07:38 +0800

    mod

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --name aleo -v ~/aleo/data:/aleo/data -p 4132:4132 -p 3032:3032 -e MINER_ADDRESS=#MINER_ADDRESS# aleo
+docker run -it --name aleo -v ./data:/aleo/data -p 4132:4132 -p 3032:3032 -e MINER_ADDRESS=#MINER_ADDRESS# qsobad/aleo
